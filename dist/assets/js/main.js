809b23ebcea4df9a2911ce4b164278ec44bfd5b8
smitevils <smitevils@yandex.ru>
2016-01-29 17:30:04 +0300

    закончил верстку главной и страници друзья

---
+++ dist/assets/js/main.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+	/* денежное меню в шапке */
+	$('.btn__money-operation-dropdown').on('click', function(event) {
+		event.preventDefault();
+		/*$(this).toggleClass('active');
+		if ($(this).is('.active')) {
+			$('.money-operation__dropdown').toggleClass('open');
+		} else {
+			$('.money-operation__dropdown').removeClass('open');
+		}*/
+		if ($(this).hasClass('active')) {
+			$('.btn__money-operation-dropdown').removeClass('active');
+			$('.money-operation__dropdown').removeClass('open');
+		} else {
+			$('.btn__money-operation-dropdown').removeClass('active');
+			$('.money-operation__dropdown').removeClass('open');
+			$(this).addClass('active');
+			var drop = $(this).data('dropdown');
+			console.log(drop)
+			$('.money-operation__dropdown[data-dropdown="'+drop+'"]').addClass('open');
+			$('.money-operation__dropdown[data-dropdown="'+drop+'"]').addClass('animated fadeInLeft');
+		}
+	});
+	$(document).mouseup(function (e){ // событие клика по веб-документу
+		var div = $(".money-operation__dropdown, .btn__money-operation-dropdown"); // тут указываем ID элемента
+		if (!div.is(e.target) // если клик был не по нашему блоку
+			&& div.has(e.target).length === 0) { // и не по его дочерним элементам
+			$('.money-operation__dropdown').removeClass('open');
+			$('#open-main-navigation').removeClass('open');
+			$('.btn__money-operation-dropdown').removeClass('active');
+		}
+	});
+
+	
+
+	/* open/close submenu */
+	$('#open-main-navigation').on('click', function(event) {
+		event.preventDefault();
+		$(this).toggleClass('open');
+		$('#main-navigation').toggleClass('open');
+		$('#main-navigation').addClass('animated fadeInLeft');
+	});
+	$(document).mouseup(function (e){ // событие клика по веб-документу
+		var div = $("#main-navigation, #open-main-navigation"); // тут указываем ID элемента
+		if (!div.is(e.target) // если клик был не по нашему блоку
+			&& div.has(e.target).length === 0) { // и не по его дочерним элементам
+			$('#main-navigation').removeClass('open');
+			$('#open-main-navigation').removeClass('active');
+		}
+	});
+
+	/* custom scroll */
+	$(".content-scrolled").mCustomScrollbar({
+		theme:"dark",
+		scrollbarPosition: "outside"
+	});
+});
